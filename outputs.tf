39380bfe790269049bcda3788e5758fdddb8f0fd
Gyanendrasr <gyanendracoder@gmail.com>
2021-02-07 08:30:58 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "endpoint" {
   description = "The IP address of the cluster master."
   sensitive   = true
-  value = google_container_cluster.default.endpoint
+  value = google_container_cluster.primary.endpoint
 }
 
 output "endpoint2" {
   description = "The IP address of the cluster master."
   sensitive   = false
-  value = google_container_cluster.default.endpoint
+  value = google_container_cluster.primary.endpoint
 }
 
 output "master_version" {
   description = "The Master Version."
   sensitive   = false
-  value = google_container_cluster.default.master_version
+  value = google_container_cluster.primary.master_version
 }
